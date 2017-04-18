33111947261688e8c1e450f0b27dfa109a6b08de
Kapil Bhagchandani <kapilb@viscus.com>
2017-04-18 16:56:51 +0530

    Request and ECAL URL process

--- src/Experian.php
+++ src/Experian.php
@@ -1,21 +1,14 @@
 <?php
 namespace Experian;
 
-use GuzzleHttp\Client;
-
-use Experian\XML;
-use Experian\Response;
+use Experian\Request;
 
 use Experian\PreQualificationReport;
 
-use Experian\Exceptions\InvalidAuth;
-use Experian\Exceptions\Unauthorized;
-use Experian\Exceptions\InvalidApp;
-
 class Experian {
-	private $client;
 
 	private $config;
+	private $request;
 
 	private $data=[];
 
@@ -23,69 +16,23 @@ class Experian {
 
 	public function __construct($config){
 		$this->config=$config;
-		$this->client = new Client();
+		$this->request=new Request($config);
 	}
 
 	public function set($key,$value){
 		$this->data[$key]=$value;
 	}
 
-	private function getResponse($products){
-		$request=[
-			'NetConnectRequest'=>[
-				'attributes'=>[
-					'xmlns'=>'http://www.experian.com/NetConnect',
-					'xmlns:xsi'=>'http://www.w3.org/2001/XMLSchema instance'
-				],
-				'EAI'=>$this->config['EAI'],
-				'DBHost'=>$this->config['DBHost'],
-				'Request'=>[
-					'attributes'=>[
-						'xmlns'=>'http://www.experian.com/WebDelivery'
-					],
-					'Products'=>$products
-				]
-			]
-		];
-		$xml=XML::encode($request);
-		$response = $this->client->request('POST', 'http://localhost/test.php', [
-			'http_errors' => false,
-			'auth' => [$this->config['username'], $this->config['password']],
-			'form_params' => [
-				'NETCONNECT_TRANSACTION' => urlencode(preg_replace('~>\s*\n\s*<~', '><', $xml))
-			]
-		]);
-
-		switch($response->getStatusCode()){
-			case 200:
-				$response=new Response($response);
-			break;
-			case 302:
-				throw new InvalidAuth;
-			break;
-			case 403:
-				throw new Unauthorized;
-			break;
-			case 404:
-				throw new InvalidApp;
-			break;
-			default:
-				throw new Exception($response->getReasonPhrase(),$response->getStatusCode());
-		}
-
-		return $response;
-	}
-
 	public function getPreQualificationReport(){
-		$requestData=PreQualificationReport::prepareRequestData($this->getARFRequest());
-		$response=$this->getResponse($requestData);
+		$products=PreQualificationReport::prepareRequestData($this->getARFRequest());
+		$response=$this->request->getARFResponse($products);
 		return PreQualificationReport::extractReport($response);
 	}
 
 	public function getARFRequest(){
 		$requestData=$this->data;
-		$requestData['Vendor']=$this->config['Vendor'];
-		$requestData['Subscriber']=$this->config['Subscriber'];
+		$requestData['Vendor']=$this->config['vendor'];
+		$requestData['Subscriber']=$this->config['subscriber'];
 		if(is_array($this->addOns)){
 			$requestData['AddOns']=[];
 			foreach($this->addOns as $addOn => $value){
---
+++ src/Request.php
@@ -0,0 +1,102 @@
+<?php
+namespace Experian;
+
+use GuzzleHttp\Client;
+use Experian\Response;
+use Experian\XML;
+
+use Experian\Exceptions\InvalidAuth;
+use Experian\Exceptions\Unauthorized;
+use Experian\Exceptions\InvalidApp;
+
+class Request{
+
+	private $client;
+	private $config;
+	private $rawResponse;
+	private $responseData;
+	private $ecalURL;
+
+	const CERT_PATH = __DIR__.'/../cert';
+
+	public function __construct($config){
+		$this->config=$config;
+		$this->client = new Client();
+	}
+
+	private function getECALUrl(){
+		$response = $this->client->request('GET','http://www.experian.com/lookupServlet1',[
+					'query'=>[
+						'lookupServiceName'=>'AccessPoint',
+						'lookupServiceVersion'=>'1.0',
+						'serviceName'=>$this->config['service_name'],
+						'serviceVersion'=>'2.0',
+						'responseType'=>'text/plain'
+					]
+				]);
+		$this->ecalURL=$response->getBody()->getContents();
+	}
+
+	public function getARFResponse($products){
+		$request=[
+			'NetConnectRequest'=>[
+				'attributes'=>[
+					'xmlns'=>'http://www.experian.com/NetConnect',
+					'xmlns:xsi'=>'http://www.w3.org/2001/XMLSchema instance'
+				],
+				'EAI'=>$this->config['eai'],
+				'DBHost'=>$this->config['db_host'],
+				'Request'=>[
+					'attributes'=>[
+						'xmlns'=>'http://www.experian.com/WebDelivery'
+					],
+					'Products'=>$products
+				]
+			]
+		];
+		$xml=XML::encode($request);
+		$response = $this->client->request('POST', $this->ecalURL, [
+			'http_errors' => false,
+			'verify' => self::CERT_PATH.'/cacert.pem',
+			'auth' => [$this->config['username'], $this->config['password']],
+			'form_params' => [
+				'NETCONNECT_TRANSACTION' => urlencode(preg_replace('~>\s*\n\s*<~', '><', $xml))
+			]
+		]);
+		switch($response->getStatusCode()){
+			case 200:
+				$response=new Response($response);
+			break;
+			case 302:
+				throw new InvalidAuth;
+			break;
+			case 403:
+				throw new Unauthorized;
+			break;
+			case 404:
+				throw new InvalidApp;
+			break;
+			default:
+				throw new Exception($response->getReasonPhrase(),$response->getStatusCode());
+		}
+
+		return $response;
+	}
+
+	/**
+	 * Install CA certificates extracted from Mozilla
+	 */
+	public static function installMozillaCACert(){
+		$client = new Client();
+		$response = $client->request('GET','https://curl.haxx.se/ca/cacert.pem',['verify' => true]);
+		if(!file_exists(self::CERT_PATH))
+			mkdir(self::CERT_PATH);
+		$f=fopen(self::CERT_PATH.'/cacert.pem','wb');
+		if(!$f) {
+			throw new \Exception('Failed to open file for saving.');
+		}
+		fwrite($f,$response->getBody()->getContents());
+		fclose($f);
+	}
+
+}
